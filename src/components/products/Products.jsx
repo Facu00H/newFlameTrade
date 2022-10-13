6536e886bcedf4ab7c80e129e8eb20aacb1ab4f5
adrieldussex <adrieldussex@gmail.com>
2022-10-13 00:06:02 -0300

    Products modified

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -19,6 +19,16 @@ export default function ProductsStore() {
             </div>
         </div>
 
+        <a href="" className='productoContenedor'>
+          <div className='productoTop'>
+            <img id='img-prdct' src="https://ithk-pro-itmall-item.oss-cn-hongkong.aliyuncs.com/2/product/0ZXSWM115021IPPX/0ZXSWM115021IPPX-pdp-1.jpg?x-oss-process=image/resize,m_pad,h_750,w_600/auto-orient,1/quality,Q_80" alt="" />
+          </div>
+          <div className='productoBot'>
+            <h3 className='productoNombre'>X Minions Shark Full Zip Hoodie</h3>
+            <p className='productoPrecio'>US$509.00</p>
+          </div>
+        </a>
+
         <div className='CardContainer'>
             <img className='img' src={'https://i.postimg.cc/NG319J1x/D-NQ-NP-715257-MLA43248628033-082020-W-removebg-preview.png'} alt='Remeras'/>
             <div>
